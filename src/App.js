597e02be4d076e0a8b8dac9ecd3223dfd91d5a33
Ramadan Emin <ramadan.a.emin@gmail.com>
2023-04-24 22:12:46 +0300

    Initial project

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import { Routes, Route } from 'react-router-dom';
+import SoundBar from './components/SoundBar/SoundBar';
+import Intro from './pages/Intro/Intro';
+import About from './pages/About/About';
+import Skills from './pages/Skills/Skills';
+import Porftolio from './pages/Porftolio/Porftolio';
+import Contact from './pages/Contact/Contact';
+
+export default function App() {
+  return (
+    <>
+      <SoundBar />
+      <Routes>
+        <Route path="/" element={<Intro />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/skills" element={<Skills />} />
+        <Route path="/portfolio" element={<Porftolio />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </>
+  );
+}
