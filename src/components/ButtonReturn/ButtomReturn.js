a932c25a661fca9fe535518f409baf4da10087c6
Ramadan Emin <ramadan.a.emin@gmail.com>
2023-04-26 13:44:02 +0300

    Added "Return button" component styling

--- src/components/ButtonReturn/ButtomReturn.js
+++ src/components/ButtonReturn/ButtomReturn.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './buttomReturn.css';
 import { NavLink } from 'react-router-dom';
 import { GiReturnArrow } from 'react-icons/gi';
 import { motion } from 'framer-motion';
