d19fdb57a95271a5f4b5643119e4a1dcb41ec33a
Ramadan Emin <ramadan.a.emin@gmail.com>
2023-04-26 14:52:53 +0300

    Added "Skills" page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Routes, Route } from 'react-router-dom';
 import SoundBar from './components/SoundBar/SoundBar';
 import Intro from './pages/Intro/Intro';
 import About from './pages/About/About';
+import Skills from './pages/Skills/Skills';
 
 export default function App() {
   return (
@@ -10,6 +11,7 @@ export default function App() {
       <Routes>
         <Route path="/" element={<Intro />} />
         <Route path="/about" element={<About />} />
+        <Route path="/skills" element={<Skills />} />
       </Routes>
     </>
   );
---
+++ src/pages/Skills/Skills.jsx
@@ -0,0 +1,82 @@
+import React from 'react';
+import NavBar from '../../components/NavBar/NavBar';
+import ButtomReturn from '../../components/ButtonReturn/ButtomReturn';
+import skills from '../../videos/skills.mp4';
+import { motion } from 'framer-motion';
+
+export default function Skills() {
+
+    const Progress = ({ done }) => {
+        const [style, setStyle] = React.useState({});
+
+        setTimeout(() => {
+            const newStyle = {
+                opacity: 1,
+                width: `${done}%`
+            };
+
+            setStyle(newStyle);
+        }, 200);
+
+        return (
+            <div className="skills_progress">
+                <div className="skills_progress-done" style={style}>
+                    {done}%
+                </div>
+            </div>
+        );
+    };
+
+    return (
+        <motion.div
+            className='skills_container'
+            variants={}
+            initial='initial'
+            animate='animate'
+        >
+            <NavBar />
+            <ButtomReturn />
+            <video autoPlay
+                muted
+                loop
+                style={{
+                    position: 'fixed',
+                }}>
+                <source src={skills} type='video/mp4' />
+            </video>
+            <motion.div
+                className='skills_box'
+                variants={}
+                initial='initial'
+                animate='animate'
+            >
+                <h1> Frond End</h1>
+                <h3 > HTML </h3>
+                <Progress done="90" />
+                <h3> CSS </h3>
+                <Progress done="90" />
+                <h3> Javascript </h3>
+                <Progress done="90" />
+                <h3> React </h3>
+                <Progress done="70" />
+            </motion.div>
+           
+            <motion.div
+                className='skills_box'
+                variants={}
+                initial='initial'
+                animate='animate'
+            >
+                <h1> Complementares</h1>
+                <h3> Redux </h3>
+                <Progress done="70" />
+                <h3> Next JS </h3>
+                <Progress done="70" />
+                <h3> Framer-Motion </h3>
+                <Progress done="90" />
+                <h3> Styled-Components </h3>
+                <Progress done="90" />
+            </motion.div>
+        </motion.div>
+    );
+} 
