034288c98cf02feb0a26df9c3b9615f4b8eae5ef
Ramadan Emin <ramadan.a.emin@gmail.com>
2023-04-27 08:44:32 +0300

    Added Data.js

---
+++ src/data/Data.js
@@ -0,0 +1,38 @@
+import fitness from '../images/fitness.png';
+import disneyPlus from '../images/disneyPlus.png';
+import metaversus from '../images/metaversus.png';
+import restaurant from '../images/restaurant.png';
+import music from '../images/music.png';
+
+export const dataPortfolio = [
+  {
+    url: `${music}`,
+    title: 'ERA music app',
+    liveDemo: 'https://era-music-app.netlify.app',
+    github: 'https://github.com/RamadanEmin/react_music_app',
+  },
+  {
+    url: `${metaversus}`,
+    title: 'Metaversus_website',
+    liveDemo: 'https://era-metaversus-website.netlify.app',
+    github: 'https://github.com/RamadanEmin/nextjs_metaversus_website',
+  },
+  {
+    url: `${fitness}`,
+    title: 'ERA fitness exercises app',
+    liveDemo: 'https://era-gym-app.netlify.app',
+    github: 'https://github.com/RamadanEmin/React_Fitness_Exercises_App',
+  },
+  {
+    url: `${restaurant}`,
+    title: 'Restaurant website',
+    liveDemo: 'https://era-responsive-restaurant-website.netlify.app',
+    github: 'https://github.com/RamadanEmin/react_responsive_restaurant_website',
+  },
+  {
+    url: `${disneyPlus}`,
+    title: 'Disney Plus clone',
+    liveDemo: 'https://disneyplus-clone-4588f.web.app/',
+    github: 'https://github.com/RamadanEmin/disneyPlus-clone',
+  },
+];
